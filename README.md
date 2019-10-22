5f650cbdf2755b1a6500d5da9a09f5c4d5ea8e06
Tapish Khandelwal <tapishkhandelwal13@gmail.com>
2019-10-22 16:39:14 +0530

    Removed Clearbit Usage & Adder Teritary Validation Sources

--- README.md
+++ README.md
@@ -16,15 +16,14 @@ To use this app locally, follow these steps:
 1. Clone the project
 2. Install requirements using `pip install -r requirements.txt`
 3. For single company search - Run `python -W ignore app.py COMPANY_NAME`
-4. For list of companies in a file -  Run `python -W ignore app.py -f  ABSOLUTE_PATH_OF_FILE`
 
 ## Sample usage
 1. ` python -W ignore app.py Microsoft `
 2. ` python -W ignore app.py "Salusive Health" `
-3. ` python -W ignore app.py -f  ~/Desktop/sample_data.txt`
 
 ### Features
 - Domain Finder
-- Fuzzy String Matching
-- Multiprocessing
-- Prediction Confidence Score
+- Fuzzy String Matching For Comparing Similiar Results
+- Non Daemonic Multiprocessing
+- Tertiary Sources Validation(Crucnhbase,Angellist,Owler,Twitter)
+- Cloudflare's IUAM Bypass
--- app.py
+++ app.py
@@ -1,106 +1,249 @@
 import requests
-import argparse
+from bs4 import BeautifulSoup,SoupStrainer
 from fuzzywuzzy import fuzz
-import os
-import time
-import pandas as pd
-from googlesearch import search
+import re
+import cfscrape
+import operator
 import multiprocessing as mp
+import argparse
+import multiprocessing.pool
+import os
 
-def getResults(line):
+cfscrape.DEFAULT_CIPHERS = 'TLS_AES_256_GCM_SHA384:ECDHE-ECDSA-'+\
+    'AES256-SHA384'
+scraper = cfscrape.create_scraper() 
 
-    company_name = line.strip().replace('\n','')
-    company = company_name.lower().replace(' ','%20')
-    flag = False
-    domain = None
-    max_score = 0
-    req = requests.get('https://autocomplete.clearbit.com/'\
-    +f'v1/companies/suggest?query={company}')
+HEADER ={
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'+\
+    '*/*;q=0.8',
+  'Accept-Language': 'en',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) '+\
+    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 '+\
+    'Safari/537.36',
+}
+POOL_COUNT = mp.cpu_count()
+class NoDaemonProcess(multiprocessing.Process):
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon)
+
+
+class MyPool(multiprocessing.pool.Pool):
+    Process = NoDaemonProcess
+
+
+def getDomainCrunchbase(link):
+    temp5 = []
+    only_a_tags = SoupStrainer('a',{'title': re.compile(r'\.')})
+    res = requests.get(link,headers=HEADER)
+    if res.status_code== 200:
+        c = res.content
+        soup = BeautifulSoup(c,'html.parser',parse_only=only_a_tags)
+        if soup.a:
+            l = soup.a.get('title')
+            if 'www.' in l:
+                l = l.split('www.')[1]
+            if '//' in l:
+                l = l.split('//')[1]
+            if '/' in l:
+                l = l.split('/')[0]
+            temp5.append(l.lower())	
+
+    return temp5
+
+def getDomainTwitter(link):
+    temp6 = []
+    only_a_tags = SoupStrainer('span',
+    {'class': 'ProfileHeaderCard-urlText u-dir'})
+    res = requests.get(link,headers=HEADER)
+    if res.status_code == 200:
+        c = res.content
+        soup = BeautifulSoup(c,'html.parser',parse_only=only_a_tags)
+        try:
+            l = soup.span.a.get('title')
+            if 'www.' in l:
+                l = l.split('www.')[1]
+            if '//' in l:
+                l = l.split('//')[1]
+            if '/' in l:
+                l = l.split('/')[0]
+            temp6.append(l.lower())	
+        except Exception as e:
+            pass
+    return temp6
+
+def getDomainOwler(link):
+    temp7 = []
+    only_a_tags = SoupStrainer('div',{'class': 'website'})	
+    c = scraper.get(link).content
+    soup = BeautifulSoup(c,'html.parser',parse_only=only_a_tags)
+    if soup.p:
+        l = soup.p.a.get('href')
+        if 'www.' in l:
+            l = l.split('www.')[1]
+        if '//' in l:
+            l = l.split('//')[1]
+        if '/' in l:
+            l = l.split('/')[0]
+        temp7.append(l.lower())	
+    return temp7
+
+def getDomainAngel(link):
+    temp8 = []
+    only_a_tags = SoupStrainer('li',{'class': 'websiteLink_daf63'})
+    c = scraper.get(link).content
+    soup = BeautifulSoup(c,'html.parser',parse_only=only_a_tags)
+    if soup.a:
+        l = soup.a.get('href')
+        if 'www.' in l:
+            l = l.split('www.')[1]
+        if '//' in l:
+            l = l.split('//')[1]
+        if '/' in l:
+            l = l.split('/')[0]
+        temp8.append(l.lower())	
+    return temp8
+
+def getDomain(i,company_search):
+    temp =[]
+    temp8=[]
+    temp5=[]
+    temp6=[]
+    temp7=[]
+    only_div_tags = SoupStrainer('div',{'class': 'r'})
+    if i==5:
+        res = requests.get('https://www.google.com/search?q='+\
+            f'{company_search}&num=20',headers=HEADER)
+        if res.status_code == 200:
+            c = res.content
+            soup = BeautifulSoup(c,'html.parser',
+            parse_only=only_div_tags)
+            for a in soup.findAll('a'):
+                link = a['href']
+                if '/search?q' not in link and link != '#':
+                    link = link.split('//')[1].split('/')[0]
+                    if 'www.' in link:
+                        link = link.split('www.')[1]
+                        if link not in temp:
+                            temp.append(link.lower())
     
-    if req.status_code == 200:
-
-        
-        data = req.json()
-
-        for i in data:
-
-            dom = str(i['domain']).split('.')[0]
-            score = fuzz.token_set_ratio(company_name,dom)
-
-            if score > max_score:
-
-                max_score=score
-                domain=i['domain'].replace('\n','')
-
-        if domain:
-
-            flag = True
+    if i==3:
+        res = requests.get('https://www.google.com/search?q='+\
+            f'crunchbase:{company_search}&num=15',headers=HEADER)
+        temp1 = []
+        if res.status_code == 200:
+            c = res.content
+            soup = BeautifulSoup(c,'html.parser',
+            parse_only=only_div_tags)
+            for a in soup.findAll('a'):
+                link = a['href']
+                if '/search?q' not in link and link != '#' and\
+				'/organization/' in link and 'crunchbase.com' in link\
+					and '/investors/' not in link:
+                    temp1.append(link.lower())
+        pool1 = multiprocessing.Pool(POOL_COUNT)
+        temp5 = pool1.map(getDomainCrunchbase,[link for link in temp1])
     
-        else:
+    if i==4:
+        res = requests.get('https://www.google.com/search?q='+\
+            f'twitter:{company_search}&num=15',headers=HEADER)
+        temp2 = []
+        if res.status_code == 200:
+            c = res.content
+            soup = BeautifulSoup(c,'html.parser',
+            parse_only=only_div_tags)
+            for a in soup.findAll('a'):
+                link = a['href']
+                if '/search?q' not in link and link != '#' and \
+                '/hashtag/' not in link and 'twitter.com' in link and \
+                    '/status/' not in link:
+                    temp2.append(link)
+        pool2 = multiprocessing.Pool(POOL_COUNT)
+        temp6 = pool2.map(getDomainTwitter,[link for link in temp2])
 
-            flag=False
-    
-    if not flag:
+    if i == 2:
+        res = requests.get('https://www.google.com/search?q='+\
+            f'owler:{company_search}&num=15',headers=HEADER)
+        temp3 = []
+        if res.status_code == 200:
+            c = res.content
+            soup = BeautifulSoup(c,'html.parser',
+            parse_only=only_div_tags)
+            for a in soup.findAll('a'):
+                link = a['href']
+                if '/search?q' not in link and link != '#' and \
+                '/company/' in link and 'owler.com' in link:
+                    temp3.append(link)
+        pool3 = multiprocessing.Pool(POOL_COUNT)
+        temp7 = pool3.map(getDomainOwler,[link for link in temp3])
 
-        response = search(company_name, stop=10,pause=1)
-        temp=[]
+    if i==1:
+        res = requests.get('https://www.google.com/search?q='+\
+            f'angellist:{company_search}&num=15',headers=HEADER)
+        temp4 = []
+        if res.status_code == 200:
+            c = res.content
+            soup = BeautifulSoup(c,'html.parser',
+            parse_only=only_div_tags)
+            for a in soup.findAll('a'):
+                link = a['href']
+                if '/search?q' not in link and link != '#'  \
+                and 'angel.co' in link and '/jobs' not in link:
+                    if '/company/' in link or '/l/' in link:
+                        temp4.append(link)
+        pool4 = multiprocessing.Pool(POOL_COUNT)
+        temp8 = pool4.map(getDomainAngel,[link for link in temp4])
 
-        for r in response:
+    dom_list = temp+temp5+temp8+temp6+temp7
+    dom_list = [x for x in dom_list if x]
+    return dom_list
 
-            if 'www.' in r:
-
-                r = r.split('www.')[1].split('/')[0]
-                
+def getResults(company_name):
+    company_search = company_name.replace(' ','%20')
+    pool0 = MyPool(5)
+    data = pool0.starmap(getDomain, 
+    [(i, company_name) for i in range(1,6)])
+    pool0.close()
+    pool0.join()
+    doms= {}
+    for i in data:
+        for j in i:
+            if isinstance(j,list):
+                if j[0] in doms:
+                    v = int(doms[j[0]])+1
+                    doms.update({j[0]:v})
+                else:
+                    doms[j[0]]=1
             else:
-                r = r.split('//')[1].split('/')[0]
+                if j in doms:
+                    v = int(doms[j])+1
+                    doms.update({j:v})
+                else:
+                    doms[j]=1
+    most_probable_domain = max(doms.items(), 
+    key=operator.itemgetter(1))[0]
 
-            if r not in temp:
-                temp.append(r)
-                score = fuzz.token_set_ratio(company_name,
-                r.split('.')[0])
-
-                if score > max_score:
-
-                    max_score=score
-                    domain=r.replace('\n','')
-
-    data = [company_name,domain,max_score]              
-    return data
+    cnt=0
+    for i in doms.keys():
+        score = fuzz.token_sort_ratio(company_name,i)
+        if int(score) >= 50:
+            cnt+=int(doms[i])
+    ratio = round(doms[most_probable_domain]/cnt,4)
+    result = [company_name,most_probable_domain,cnt,ratio]
+    return result
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(
-        description="Find domain name with the help of comapany name")
+		description="Find domain name with the help of comapany name")
     parser.add_argument("company_name",help="Enter a company name",
-    type=str)
-    parser.add_argument('-f','--filepath',help='Enter a file path',
-    action="store_true")
+	type=str)
     args = parser.parse_args()
-    
-
-    if not args.filepath:
-
+    if args.company_name:
         company_name = args.company_name
-        data = getResults(company_name)
-        print(f'Company Name -> {data[0]} \t Domain -> {data[1]} '\
-            +f'\t Prediction Confidence Score {data[2]}')
-
-    else:
-
-        file_path = os.path.abspath(args.company_name)
-        dom_data = []
-        pool = mp.Pool(mp.cpu_count())
-
-        with open(file_path) as source_file:
-
-            results = pool.map(getResults, source_file, 1)
-
-        pool.close()
-        pool.join()
-
-        df_domains = pd.DataFrame(data=results)
-        df_domains.columns = ['Company Name','Domain',
-        'Prediction Confidence Score']
-        df_domains.to_csv(str(f'results_{int(time.time())}'),sep='\t')
-        df_domains.to_json(str(f'results_{int(time.time())}_json'),
-        orient='records')
+        result = getResults(company_name)
+        print(f'Company Name -> {result[0]} \t Domain -> {result[1]} '\
+            +f'\t Similar Domains Count -> {result[2]} \t '+\
+                f'Probability -> {result[3]}')
